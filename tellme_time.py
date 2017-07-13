3d6f002959959dfb83aba9b010ba69a2e6fcc5aa
ev-glitch <evalyn.mail@gmail.com>
2017-07-13 15:04:21 -0400

    working on tellme_time.py data model.

--- tellme_time.py
+++ tellme_time.py
@@ -9,13 +9,13 @@ print("The time is now: ", time.time())
 print("The local time: ", time.localtime())
 
 # Time only runs in one direction
+monotime = 0
+systime = 0
 old_monotime = 0
 old_systime = 0
 output = ""
 
-column_names = [ "Monotime", "System Time", "Mono Delta", "System Delta" ] 
-column_gen = ["time.monotonic()", 
-              "time.time()" ]
+# column_names = [ "Monotime", "System Time", "Mono Delta", "System Delta" ] 
 
 border = "||"
 column_sep = "||"
@@ -24,14 +24,19 @@ column = { "Monotime": "time.monotonic()", "Systime": "time.time()",
            "Mono Delta": "monotime - old_monotime", 
            "System Delta": "systime - old_systime"}
 
+column_temp = []
+
 print("||  Monotime  ||  System Time  ||  Mono Delta  ||  System Delta  ||")
 print("===================================================================")
 
 while True:
     time.sleep(5)
     output = "|| "
-    for gen in column_gen: 
-        output += str(eval(gen)) + " || "
+    for key in column.keys(): 
+        column_temp.append( (key, eval(column[key])) )
+    print(column_temp)
+    column_temp = []
+    # output += str(eval(column[key])) + " || "
    # monotime = eval(column_gen[0])
    # systime = eval(column_gen[1])
 
